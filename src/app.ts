2fa152d4c12380545e9aafab3b9779c1e682ef16
Vanya1000 <Vanya1000@yandex.ru>
2023-06-02 21:14:26 +0300

    feat: implement cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import renderErrors from './routes/render-errors';
 import dbConnector from './plugins/db-connector';
+import cors from '@fastify/cors';
 import 'dotenv/config';
 
 const port = process.env.PORT as unknown as number;
@@ -9,12 +10,16 @@ const fastify = Fastify({
   logger: false,
 });
 
+fastify.register(cors, {
+  origin: '*',
+});
+
 fastify.register(dbConnector);
 fastify.register(renderErrors);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: port });
+    await fastify.listen({ port: port, host: '0.0.0.0' });
     console.log(`start on port ${port}`);
   } catch (err) {
     fastify.log.error(err);
